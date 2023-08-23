b6b08305ad253520f103752a5ffefc6a1488a88d
Valentin Kharin <valentin.kharin@unikie.com>
2023-08-23 13:50:05 +0300

    Move installer generator to lib
    Signed-off-by: Valentin Kharin <valentin.kharin@unikie.com>

--- targets/installer.nix
+++ targets/installer.nix
@@ -9,49 +9,8 @@
   lib,
 }: let
   formatModule = nixos-generators.nixosModules.raw-efi;
-  installer = {name, systemImgCfg, modules ? []}: let
-    system = systemImgCfg.config.nixpkgs.hostPlatform.system;
-
-    pkgs = import nixpkgs {inherit system;};
-    systemImgDrv = systemImgCfg.config.system.build.${systemImgCfg.config.formatAttr};
-
-    installerScript = import ../modules/installer/installer.nix { inherit pkgs; systemImgDrv = "${systemImgDrv}/nixos.img";  inherit (pkgs) runtimeShell; };
-
-    installerImgCfg = lib.nixosSystem {
-      inherit system;
-      specialArgs = {inherit lib;};
-      modules =
-        [
-          ../modules/host
-
-          ({modulesPath, ...}: {
-            imports = [ (modulesPath + "/profiles/all-hardware.nix") ];
-
-            nixpkgs.hostPlatform.system = system;
-            nixpkgs.config.allowUnfree = true;
-
-            hardware.enableAllFirmware = true;
-
-            ghaf.profiles.installer.enable = true;
-          })
-
-          {
-            # TODO
-            environment.loginShellInit = ''
-              ${installerScript}/bin/ghaf-installer
-            '';
-          }
-
-          formatModule
-        ]
-        ++ (import ../modules/module-list.nix) ++ modules;
-    };
-  in {
-    name = "${name}-installer";
-    inherit installerImgCfg system;
-    installerImgDrv = installerImgCfg.config.system.build.${installerImgCfg.config.formatAttr};
-  };
-  targets = map installer [{name = "generic-x86_64-release"; systemImgCfg = self.nixosConfigurations.generic-x86_64-release;}];
+  inherit (lib.ghaf) installer;
+  targets = map installer [{name = "generic-x86_64-release"; systemImgCfg = self.nixosConfigurations.generic-x86_64-release; modules = [formatModule];}];
 in {
   packages = lib.foldr lib.recursiveUpdate {} (map ({name, system, installerImgDrv, ...}: {
     ${system}.${name} = installerImgDrv;
