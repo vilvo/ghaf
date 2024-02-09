f5e9e313e669e227292c7d1b180c2097baa76a34
Ville Ilvonen <ville.ilvonen@unikie.com>
2024-02-09 09:16:16 +0200

    clean: remove stale comments and update
    * "allowImportFromDerivation = false" removed as it's
      the default value. Allow (true) made the nix flake
      show fail earlier but allowed bypassing NixOS asserted
      kernel config options from the baseline config instead
      of inline
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- packages/kernel/default.nix
+++ packages/kernel/default.nix
@@ -16,7 +16,11 @@
     {
       inherit (kernel_package) src;
       inherit version modDirVersion kernelPatches;
-      allowImportFromDerivation = false;
+      /* NixOS required (asserted) kernel features
+         to comply with no import from derivation
+         for the actual kernel build these config
+         options must come via the kernel
+         config_baseline argument */
       config = {
         CONFIG_DEVTMPFS = "y";
         CONFIG_CGROUPS = "y";
@@ -43,50 +47,6 @@
         CONFIG_BINFMT_ELF = "y";
         CONFIG_UNIX = "y";
       };
-      /*
-      baseline "make tinyconfig"
-      - enabled for 64-bit, TTY, printk and initrd
-      - fixed following NixOS required assertions via "make menuconfig" + search
-      (following is documented here to highlight NixOS required (asserted) kernel features)
-      ❯ nix build .#packages.x86_64-linux.lenovo-x1-carbon-gen11-debug --accept-flake-config
-      error:
-         Failed assertions:
-         - CONFIG_DEVTMPFS is not enabled!
-         - CONFIG_CGROUPS is not enabled!
-         - CONFIG_INOTIFY_USER is not enabled!
-         - CONFIG_SIGNALFD is not enabled!
-         - CONFIG_TIMERFD is not enabled!
-         - CONFIG_EPOLL is not enabled!
-         - CONFIG_NET is not enabled!
-         - CONFIG_SYSFS is not enabled!
-         - CONFIG_PROC_FS is not enabled!
-         - CONFIG_FHANDLE is not enabled!
-         - CONFIG_CRYPTO_USER_API_HASH is not enabled!
-         - CONFIG_CRYPTO_HMAC is not enabled!
-         - CONFIG_CRYPTO_SHA256 is not enabled!
-         - CONFIG_DMIID is not enabled!
-         - CONFIG_AUTOFS4_FS is not enabled!
-         - CONFIG_TMPFS_POSIX_ACL is not enabled!
-         - CONFIG_TMPFS_XATTR is not enabled!
-         - CONFIG_SECCOMP is not enabled!
-         - CONFIG_TMPFS is not yes!
-         - CONFIG_BLK_DEV_INITRD is not yes!
-         - CONFIG_EFI_STUB is not yes!
-         - CONFIG_MODULES is not yes!
-         - CONFIG_BINFMT_ELF is not yes!
-         - CONFIG_UNIX is not enabled!
-         - CONFIG_INOTIFY_USER is not yes!
-         - CONFIG_NET is not yes!
-      ...
-      additional NixOS dependencies (fixed):
-      > modprobe: FATAL: Module uas not found ...
-      > modprobe: FATAL: Module nvme not found ...
-      ... < many packages enabled as M,
-            others allowMissing = true with overlay
-            - see implementation below under cfg.enable
-      - also see https://github.com/NixOS/nixpkgs/issues/109280
-        for the context >
-      */
       configfile = config_baseline;
     };
 
@@ -94,8 +54,6 @@
   generic_guest_configs = ../../modules/hardware/x86_64-generic/kernel/guest/configs;
   # TODO: refactor - do we yet have any X1 specific host kernel configuration options?
   # - we could add a configuration fragment for host debug via usb-ethernet-adapter(s)
-  # TODO: refactor config paths to
-  # x1_host_source = ../../modules/hardware/lenovo-x1/host/configs;
 
   kernel_features =
     lib.optionals config.ghaf.host.kernel.virtualization_hardening.enable ["${generic_host_configs}/virtualization.config"]
